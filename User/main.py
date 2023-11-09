2452a8b562f17a15b23f3cef5a5f29e465f9ec6a
Raed-93 <tbilhraed@gmail.com>
2023-11-09 13:04:45 +0200

    raed2

--- User/main.py
+++ User/main.py
@@ -1,26 +1,35 @@
-class user:
-    def __init__(self,name,balance):
+class User:
+    def __init__(self, name, balance):
         self.name = name
         self.balance = balance
 
     def make_withdrawal(self, amount):
-        if amount > 0 and amount >= self.balance:
-         self.balacne -= amount
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
 
     def display_user_balance(self):
-       print("User: %s, Balance:%d" % (self.name, self.balance))
+        print("User: %s, Balance: %d" % (self.name, self.balance))
+
+    def transfer_money(self, another_user, amount):
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            another_user.balance += amount
+            print("Transferred: %d to %s" % (amount, another_user.name))
+        else:
+            print("Transfer not possible")
+
+user1 = User("Raed", 200000)
+user2 = User("Mohamad", 10000)
+
+user1.display_user_balance()
+user2.display_user_balance()
+
+user1.transfer_money(user2, 50)
+user1.display_user_balance()
+user2.display_user_balance()
+user1.make_withdrawal(50)
 
 
-    def transfer_money(self,amount,antheruser):
-       if amount > 0 and amount >= self.balance:
-         self.balacne -= amount
-         antheruser.balance += amount
-         print("transfer: %d, name: %s" % (amount,self.name))
-       else:
-          print("none")
-
-          user1 = user("name","balance")
-        
     
           
 
